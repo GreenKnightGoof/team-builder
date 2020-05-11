3105dff0194583b755385adac08220f27b6768ac
GreenKnightGoof <60906118+GreenKnightGoof@users.noreply.github.com>
2020-05-11 17:05:36 -0500

    Form is close, about to change some things.

--- src/Form.js
+++ src/Form.js
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Form.css";
 
 function Form(props) {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [role, setRole] = useState("");
+
   return (
     <div className="form-container">
       <form
@@ -16,7 +20,32 @@ function Form(props) {
           type="text"
           name="name"
           placeholder="Please Enter Your Name:"
-          value={"Kyle"}
+          value={name}
+          onChange={(event) => {
+            setName(event.target.value);
+          }}
+        ></input>
+        <label htmlFor="email">Email:</label>
+        <input
+          id="email"
+          type="text"
+          name="email"
+          placeholder="Please Enter Your Email:"
+          value={email}
+          onChange={(event) => {
+            setEmail(event.target.value);
+          }}
+        ></input>
+        <label htmlFor="role">Role:</label>
+        <input
+          id="role"
+          type="text"
+          name="role"
+          placeholder="Please Enter Your Role:"
+          value={role}
+          onChange={(event) => {
+            setRole(event.target.value);
+          }}
         ></input>
         <button type="submit">Click to Submit!</button>
       </form>
